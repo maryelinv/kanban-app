74f014abb3c5726e85351e5253dd88caed3a2a13
Maryelin Ramirez <mralvarez@banreservas.com>
2025-09-29 17:40:13 -0400

    chore: generate Angular app with CLI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { kanbanReducer, KANBAN_FEATURE_KEY } from './kanban/store/reducer';
+import { KanbanEffects } from './kanban/store/effects';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore(),
+    provideState(KANBAN_FEATURE_KEY, kanbanReducer),
+    provideEffects([KanbanEffects]),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'kanban', pathMatch: 'full' },
+  {
+    path: 'kanban',
+    loadComponent: () =>
+      import('./kanban/components/board/board.component').then(m => m.BoardComponent)
+  },
+];
